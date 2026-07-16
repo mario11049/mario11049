2e5b57ce85477c06653ea10706cb82fd06dff90c
Mario_Manoj_ <30056viz@tirumalajc.edu.in>
2026-07-16 12:54:25 +0000

    testing collaburation

--- test space/validate.py
+++ test space/validate.py
@@ -1,7 +1,2 @@
-num = int(input("enter the number you want to reverse: "))
-temp = 0
-while num > 0:
-    digit = num % 10
-    temp = temp * 10 + digit
-    num = num // 10
-print(temp)
+name = input("whats your name: ")
+print(f"your name is {name}")
