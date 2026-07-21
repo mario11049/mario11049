75cc9bebb0ca50890b729b7bfbea732c140cf085
Mario_Manoj_ <30056viz@tirumalajc.edu.in>
2026-07-21 08:08:11 +0000

    __init__

---
+++ test space/main.py
@@ -0,0 +1,5 @@
+def hellow():
+    print("hellow")
+
+if __name__ =="__main__":
+    hellow()
--- test space/student.py
+++ test space/student.py
@@ -1,13 +1,22 @@
+class Student:
+    def __init__(self , name, house):
+        if not name:
+            raise ValueError("enter name")
+        if house not in {"gryffindor", "slytherine", "ravanclaw"}:
+            raise("invalid house")
+        self.name = name
+        self.house = house
+    
+
 def main():
     student = get_student()
-    if student["name"] == "manoj":
-        student["home"] = "tgv"
-    print(f"{student['name']} is from {student['home']}")
+    print(f"{student.name} from {student.house}")
 
 def get_student():
     name = input("name: ")
-    home = input("home: ")
-    return {"name":name, "home":home}
+    house = input("home: ")
+    student = Student(name, house)
+    return student
 
 if __name__ == "__main__":
     main()
