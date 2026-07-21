290ee46f1f3558494e72cad14de338a7d74be1de
Mario_Manoj_ <30056viz@tirumalajc.edu.in>
2026-07-21 17:45:38 +0000

    added charm

--- test space/student.py
+++ test space/student.py
@@ -1,18 +1,31 @@
 class Student:
-    def __init__(self , name, house):
+    def __init__(self , name, house, patronus):
         self.name = name
         self.house = house
+        self.patronus = patronus
     def __str__(self):
         return(f"{self.name} from {self.house}")
+    def charm(self):
+        match self.patronus:
+            case "stag":
+                return"🐴"
+            case "otter":
+                return"🥾"
+            case "mario":
+                return"🕹️"
+            case _:
+                return"🙏🏻"
 
 def main():
     student = get_student()
     print(student)
+    print(student.charm())
 
 def get_student():
     name = input("name: ")
     house = input("home: ")
-    student = Student(name, house)
+    patronus = input("patronus: ")
+    student = Student(name, house, patronus)
     return student
 
 if __name__ == "__main__":
